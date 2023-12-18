d57e4fd2e9aed6c0529c0dc4833f075dfb0374f0
vladimir <vstrukoff@yandex.ru>
2023-12-18 08:04:07 +0500

    Убрал ненужную библиотеку

--- project/shared/my-library/src/lib/shared-my-library.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedMyLibrary } from './shared-my-library';
-
-describe('sharedMyLibrary', () => {
-  it('should work', () => {
-    expect(sharedMyLibrary()).toEqual('shared-my-library');
-  });
-});
--- project/shared/my-library/src/lib/shared-my-library.ts
+++
@@ -1,3 +0,0 @@
-export function sharedMyLibrary(): string {
-  return 'shared-my-library';
-}
