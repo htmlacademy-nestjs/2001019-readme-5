c124c5ce689142951653c1730389f40140e65a6d
vladimir <vstrukoff@yandex.ru>
2023-12-19 07:44:07 +0500

    Зарегистрировали репозиторий как провайдер

--- project/apps/account/src/app/blog-user/blog-user.module.ts
+++ project/apps/account/src/app/blog-user/blog-user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogUserController } from './blog-user.controller';
 import { BlogUserService } from './blog-user.service';
+import {BlogUserRepository} from "./blog-user.repository";
 
 @Module({
-  controllers: [BlogUserController],
-  providers: [BlogUserService],
+  controllers: [BlogUserRepository],
+  providers: [BlogUserRepository],
 })
 export class BlogUserModule {}
