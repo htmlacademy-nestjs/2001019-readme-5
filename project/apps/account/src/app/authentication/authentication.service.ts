0323f41a32f8203502030f1576bb054fb8cd5340
vladimir <vstrukoff@yandex.ru>
2023-12-19 07:53:33 +0500

    Добавили метод регистрации

--- project/apps/account/src/app/authentication/authentication.service.ts
+++ project/apps/account/src/app/authentication/authentication.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {ConflictException, Injectable} from '@nestjs/common';
 import {BlogUserRepository} from "../blog-user/blog-user.repository";
+import {CreateUserDto} from "./dto/create-user.dto";
+import dayjs from "dayjs";
+import {UserRole} from "@project/types";
+import {BlogUserEntity} from "../blog-user/blog-user.entity";
+import {AUTH_USER_EXISTS} from "./authentification.constant";
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, firstname, lastname, password, dateBirth} = dto;
+
+    const blogUser = {
+      email, firstname, lastname, role: UserRole.User,
+      avatar: '', dateOfBirth: dayjs(dateBirth).toDate(),
+      passwordHash: ''
+    };
+
+    const existUser = await this.blogUserRepository
+      .findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+
+    return this.blogUserRepository
+      .save(userEntity);
+  }
 }
