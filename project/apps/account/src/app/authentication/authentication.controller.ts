4eb578e78a3103197c787a1de259c7bc0a3636bf
vladimir <vstrukoff@yandex.ru>
2023-12-19 08:02:27 +0500

    Метод регистрации в auth контроллере

--- project/apps/account/src/app/authentication/authentication.controller.ts
+++ project/apps/account/src/app/authentication/authentication.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
 import {AuthenticationService} from "./authentication.service";
+import {CreateUserDto} from "./dto/create-user.dto";
+import {UserRdo} from "./rdo/user.rdo";
+import {fillDto} from "../../../../../shared/helpers/src/lib/common";
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthenticationService
   ) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillDto(UserRdo, newUser.toPOJO());
+  }
 }
