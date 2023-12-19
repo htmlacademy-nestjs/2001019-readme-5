e3cf9703d54c0334ec2144a5c1b2d00b9fbecbfb
vladimir <vstrukoff@yandex.ru>
2023-12-19 07:42:05 +0500

    Добавил BlogUserRepository

---
+++ project/apps/account/src/app/blog-user/blog-user.repository.ts
@@ -0,0 +1,4 @@
+import { BaseMemoryRepository } from '@project/core';
+import { BlogUserEntity } from './blog-user.entity';
+
+export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {}
